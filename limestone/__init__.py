635325eb15b7b3ac9afb8086b1268df31ca4045a
Mahmoud Emara <112200088+MahmoudbEmara@users.noreply.github.com>
2025-10-24 11:24:02 +0300

    Update __init__.py

--- limestone/__init__.py
+++ limestone/__init__.py
@@ -1,15 +1,8 @@
 from flask import Flask
 from .database import init_db
-from .routes.auth_routes import auth_bp
-from .routes.update_routes import update_bp
-from .routes.dashboard_routes import dashboard_bp
-from .routes.dailytrend_routes import dailytrend_bp
-from .routes.history_routes import history_bp
-from .routes.api import api_bp
 import os
 
 def create_app():
-    # Tell Flask where to find templates and static files
     app = Flask(
         __name__,
         template_folder=os.path.join(os.path.dirname(__file__), "templates"),
@@ -18,10 +11,15 @@ def create_app():
 
     app.config.from_object("config.Config")
 
-    # Initialize DB
     init_db(app)
 
-    # Register blueprints
+    from .routes.auth_routes import auth_bp
+    from .routes.update_routes import update_bp
+    from .routes.dashboard_routes import dashboard_bp
+    from .routes.dailytrend_routes import dailytrend_bp
+    from .routes.history_routes import history_bp
+    from .routes.api import api_bp
+
     app.register_blueprint(auth_bp)
     app.register_blueprint(update_bp)
     app.register_blueprint(dashboard_bp)
