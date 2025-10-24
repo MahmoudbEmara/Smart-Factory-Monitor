b4c850638e7c4b413aa56a3652b37e72693d9d83
Mahmoud Emara <112200088+MahmoudbEmara@users.noreply.github.com>
2025-10-24 12:23:11 +0300

    Update __init__.py

--- limestone/__init__.py
+++ limestone/__init__.py
@@ -15,14 +15,14 @@ def create_app():
 
     from .routes.auth_routes import auth_bp
     from .routes.update_routes import update_bp
-    from .routes.dashboard_routes import dashboard_bp
+    # from .routes.dashboard_routes import dashboard_bp
     from .routes.dailytrend_routes import dailytrend_bp
     from .routes.history_routes import history_bp
     from .routes.api import api_bp
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(update_bp)
-    app.register_blueprint(dashboard_bp)
+    # app.register_blueprint(dashboard_bp)
     app.register_blueprint(dailytrend_bp)
     app.register_blueprint(history_bp)
     app.register_blueprint(api_bp)
